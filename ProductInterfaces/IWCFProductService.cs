ce2e95742351b4249a98b348ec872325fe982adc
pkirklewski <kirklewskis@gmail.com>
2017-09-13 11:54:24 +0100

    ProductService.IWCFProductService - Contract problem fixed

--- ProductInterfaces/IWCFProductService.cs
+++ ProductInterfaces/IWCFProductService.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.ServiceModel;
 
-namespace ProductService
+namespace ProductInterfaces
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWCFProductService" in both code and config file together.
     [ServiceContract]
